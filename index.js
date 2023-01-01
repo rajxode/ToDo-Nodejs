2ac9d6455fc358e8c3cb1babc179a058366577ad
Rajeev <rajsh1202@gmail.com>
2023-01-01 20:51:58 +0530

    added router and controller

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // importing express into our file 
 const express=require('express');
 
-const path = require('path');
-
 // defining port number for our server
 const port=8000;
 const app=express();
@@ -12,10 +10,7 @@ app.use('/',require('./routes/index'));
 
 // setting view engine as ejs 
 app.set('view engine','ejs');
-
-
-// defining path of result file with respect to current file
-app.set('views', path.join(__dirname, 'views'));
+app.set('views','./views');
 
 
 // running server
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
+// importing express
 const express = require('express');
 
+// setting up the route
 const router = express.Router();
+
+// importing controller for home 
+const homeController = require('../controllers/home_controller');
 
 console.log('Routers running');
 
+// using controller for corresponding routes
+router.get('/',homeController.home);
+
+
+// exporting router for further use
 module.exports = router;
