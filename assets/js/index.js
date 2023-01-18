e23135707f69292d1b501ef4b0c341e6e8f4f7d3
Rajeev <rajsh1202@gmail.com>
2023-01-18 21:21:32 +0530

    Added styles in home html file

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+var checkbox=document.getElementById('work_status');
+var description=document.getElementById('task_description')
+
+checkbox.addEventListener('click',function(){
+    console.log('clicked');
+    return;
+})
